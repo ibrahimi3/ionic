fafecb71e6af656d7a00a64fbf42a22785426d1a
ibrahimi3 <ibrahimisa359@gmail.com>
2018-01-15 13:45:35 +0100

    'update'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,32 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
-  declarations: [
+declarations: [
     MyApp,
     HomePage
-  ],
-  imports: [
+],
+imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
+],
+bootstrap: [IonicApp],
+entryComponents: [
     MyApp,
     HomePage
-  ],
-  providers: [
+],
+providers: [
     StatusBar,
     SplashScreen,
+    InAppBrowser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+]
 })
 export class AppModule {}
+
